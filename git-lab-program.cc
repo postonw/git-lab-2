a8cf10bc2d326127e347a495021f70edd1f5162f
William Poston <wp826418@ohio.edu>
2019-05-15 21:26:07 -0400

    made hello git program in git-lab-2

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: William Poston
+ *        Date: May 15, 2019
+ * Description: Homework 1 say hello git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+//function prototypes
+
+int main(int argc, char const *argv[]) {
+
+   /*add code*/
+   cout << "Hello Git!" << endl;
+   return 0;
+}// main
